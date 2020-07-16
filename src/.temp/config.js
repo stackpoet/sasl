7c7cfa2afb439d7fd97177c128f340254df3d9f0
Kenya Sullivan <kenya@stackpoets.com>
2020-07-15 20:21:31 -0400

    clean repo

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - Docc",
-  "siteUrl": "https://example.com",
-  "version": "0.7.19",
-  "catchLinks": true
-}
